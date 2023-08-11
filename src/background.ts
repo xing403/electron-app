7e76b2beb40cd4091dddb5bf9bf15da9eb940906
星如雨 <liyun200104@163.com>
2023-08-11 22:37:01 +0800

    :art: show two windows and drag frame false

--- src/background.ts
+++ src/background.ts
@@ -1,28 +1,40 @@
 // Electron entry file
 import { app, BrowserWindow, ipcMain } from 'electron';
-import path from 'node:path'
+import type { BrowserWindowConstructorOptions } from 'electron';
+import path from 'path'
 
-const createWindow = (width = 1400, height = 1000) => {
+
+const createWindow = (width = 1400, height = 1000, config?: BrowserWindowConstructorOptions) => {
   const win = new BrowserWindow({
     width,               // 桌面程序的宽度
     height,              //    ... 高度
+    parent: config?.parent ?? undefined,   // 父窗口
+    modal: config?.modal ?? false,        // 模态窗口
+    show: config?.show ?? true,         // 是否显示
+    frame: config?.frame ?? true,        // 是否显示边框
     webPreferences: {
-      // nodeIntegration: true,        // 在渲染过成功使用node api
-      // contextIsolation: false,       // 上下文隔离
-      // webSecurity: false,         // 浏览器跨域检测
       preload: path.join(__dirname, './preload.js'),
     },
   });
-  win.webContents.openDevTools(); // 打开窗口的调试工具
+  //  win.webContents.openDevTools(); // 打开窗口的调试工具
   return win
 }
 app.whenReady().then(() => {
-  const win = createWindow()
+  const mainWindow = createWindow(800, 800)
+  const secondWindow = createWindow(600, 600, {
+    parent: mainWindow,
+    modal: true,
+    frame: false
+  })
+
   if (process.argv[2]) {
-    win.loadURL(process.argv[2])  // 开发环境
+    mainWindow.loadURL(process.argv[2])  // 开发环境
   } else {
-    win.loadFile('index.html')  // 生产环境
+    mainWindow.loadFile('index.html')  // 生产环境
   }
+
+  secondWindow.loadURL(`http://localhost${process.argv[2].split('localhost')[1]}/#/login`)
+
   ipcMain.handle("send-message-main", (_event, data: any) => {
     console.log(_event, data)
     return "main receive the message";
--- src/preload.ts
+++ src/preload.ts
@@ -5,16 +5,6 @@ const sendMessageMain = async (data: any) => {
   let fallback =  await ipcRenderer.invoke("send-message-main", data);
   return fallback;
 };
-
-// 可以将主进程中的一些变量注入到渲染进程中，"plotform" 为自定义名称
-// contextBridge.exposeInMainWorld("plotform", {
-//   platform: "electron",
-//   version: process.versions.electron,
-//   func: () => {
-//     console.log("electron");
-//   }
-// });
 contextBridge.exposeInMainWorld("API", {
   sendMessageMain
 });
-// 在 *.vue 或者 *.html 中使用 windows.plotform 即可，查看到注入的值
